6a155cb80b9a4cb2cd296acaca8d619976b07c04
Filipe André <flipeandre12@gmail.com>
2023-12-06 18:21:54 -0300

    docs: adicionando o faker ao template

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,7 @@
+import { faker }from '@faker-js/faker';
 describe('template spec', () => {
   it('passes', () => {
+    const email = faker.internet.email();
     cy.visit('https://example.cypress.io')
   })
 })
